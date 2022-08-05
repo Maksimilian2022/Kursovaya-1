8cea9dea28a31716767e6bb7d7f4c4657c59e937
Maksimilian Romanov <maks.romanov.15@inbox.ru>
2022-08-05 17:01:15 +0300

    add kursovaya #1.1.1

--- Kursovaya.py
+++ Kursovaya.py
@@ -1,36 +1,35 @@
-
-from pprint import pprint
+import json
 import requests
 
+access_token = 'vk1.a.FOPeogRotD8mxzUyIs1li1ENNH19zPZRy1nWnJHcR8Bj6nHOQSyraeCCtgNEJbZc4a1ZZVaAQVq7TYIM37R-VIKGMJ59PBLMbodtOhflLfv_r7iPM8R0DZukNT6fe6fCqu_oBnUXz0v_v25EgT9z_ofRhJfBq1Oeub81djXC0cs6dINYLX3OJ5OSUDCXQTCv'
 
-def get_photo():
-    photos_dict ={}
+
+def get_photo(user_id):
+    photos_dict = {}
     url = 'https://api.vk.com/method/photos.get'
-    params = {'owner_id': '1', 'album_id': 'wall', 'v': '5.81', 'access_token': access_token, 'extended': '1', 'count': '4', 'photo_sizes': '1'  }
+    params = {'owner_id': user_id, 'album_id': 'wall', 'v': '5.81', 'access_token': access_token, 'extended': '1',
+              'count': '4', 'photo_sizes': '1'}
     response = requests.get(url, params=params)
     for photo_max_size in response.json()['response']['items']:
-
-        photos_dict[photo_max_size['likes']['count']] = photo_max_size['sizes'][-1]['url']
-        if photos_dict[photo_max_size['likes']['count']] == photos_dict:
-            photos_dict[photo_max_size['likes']['count']] = photo_max_size['data']
+        photos_dict[photo_max_size['sizes'][-1]['url']] = [
+            {"file_name": photo_max_size['likes']['count'], "size": photo_max_size['sizes'][-1]['type']}]
 
     with open('Photos', 'w') as open_file:
-        open_file.write(str(photos_dict))
+        json.dump(photos_dict, open_file)
     a = photos_dict.values()
     return a
 
 
-def post_ydisk():
+def post_ydisk(id):
     token = 'AQAAAABR6sW2AADLW-hiYhDRWkXIgVyXB5LPvQQ'
     y_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-    params = {'path': 'Vk', 'url': get_photo()}
+
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-    for photo_post in get_photo():
+    for photo_post in get_photo(1):
+        print(photo_post)
         params = {'path': 'Vk', 'url': photo_post}
         put_resource = requests.post(url=y_url, params=params, headers=headers)
-
     return
-print(post_ydisk())
 
 
 
